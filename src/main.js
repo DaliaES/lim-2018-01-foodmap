91acedd1c0d8fd9a818baf61e6f777b1b6465259
DaliaES <dalia.escuderov@gmail.com>
2018-08-11 17:31:57 -0500

    modal

--- src/main.js
+++ src/main.js
@@ -7,34 +7,50 @@ const ListRestaurants = (value) => {
 				const objectRest = dataRest[element]
 				objectRest.forEach(element => {
 					containerName.innerHTML += `
-    <div class="col l3 s12 m6" data-value = "" class= "rest">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <h2 class="center name">${element.name}</h2>
+    <div class="col l3 s12 m6 info-rest" class= "rest"  onclick="showModal('${element.name}')">
+      <div class="card yellow lighten-3">
+				<div class="card-content white-text">
+				<a class="modal-trigger indigo-text text" href="#modalInfo">
+        ${element.name}</a>
         </div>
       </div>
     </div>
 					`
-					// containerName.innerHTML = cardRest + containerName.innerHTML
 				});
 			}
 		}
 	});
 }
+const containerInfo= document.getElementsByClassName("info-rest");
 const selectRest = document.getElementById("select-type")
 selectRest.addEventListener('change', e => {
 	containerName.innerHTML = ''
 	ListRestaurants(e.target.value)
 })
-const showModal = () => {
+const showModal = (name) => {
 	xhrData(urlRestaurantes, () => {
 		const dataRest = getData()
-		for (element in dataRest) {
-			const objectRest = dataRest[element]
+		for (element1 in dataRest) {
+			const objectRest = dataRest[element1]
 			objectRest.forEach(element => {
-				console.log(element.name)
+				if (element.name===name){
+					ShowInfo(element.name,element.direccion,element.horario)
+				}
+
 			});
 		}
 	});
-	console.log('hola')
 }
+const ShowInfo =(name,direction,horario)=>{
+const nameRest = document.getElementById("name-rest")
+const infoRest = document.getElementById("info-rest")
+nameRest.innerHTML = `<h3>${name}</h3>`
+infoRest.innerHTML= `
+<h5>Dirección: ${direction}</h5>
+<h5>Horario de atención: ${horario}</h5>
+`
+}
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.modal');
+	var instances = M.Modal.init(elems, 'opacity');
+});
